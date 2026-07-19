2c268a5d6871739ee0048f2465f93899a68be4fb
Jana Alhumaidan <122381274+JanaAlhumaidan@users.noreply.github.com>
2026-07-19 15:50:14 +0300

    fixed the right alignment

--- obj/Debug/net10.0/AIChatbot.AssemblyInfo.cs
+++ obj/Debug/net10.0/AIChatbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9599eab758a5f27dbbd8a4e7e1ea9b046a1b2a16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+940bbaea217de8e7f92e0cf1f2911c6621512e46")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
